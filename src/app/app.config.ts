d187490087fdf58a4cb8e6bed02ec4a743810b8d
Engels Acuña <engels.taipe0530@gmail.com>
2024-11-17 06:31:41 -0500

    Ejercicio 4 añadido

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,3 +7,21 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
 };
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';  // Importa BrowserModule para usar directivas como *ngIf y *ngFor
+import { AppComponent } from './app.component';  // Tu componente principal
+import { Ejercicio4Component } from './ejercicio4/ejercicio4.component';  // Importa tu componente Ejercicio4
+
+@NgModule({
+  declarations: [
+    
+    Ejercicio4Component  // Asegúrate de declarar tu componente Ejercicio4
+  ],
+  imports: [
+    BrowserModule  // Importa BrowserModule para habilitar directivas como *ngIf
+  ],
+  providers: [],
+  bootstrap: []  // Tu componente raíz
+})
+export class AppModule { }
